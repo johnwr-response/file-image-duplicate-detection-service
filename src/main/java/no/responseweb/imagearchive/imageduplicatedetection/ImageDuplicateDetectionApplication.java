1c3ceeffd930dc219f03d98eae253b7c02ea6ace
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2020-07-01 17:39:55 +0200

    init

---
+++ src/main/java/no/responseweb/imagearchive/imageduplicatedetection/ImageDuplicateDetectionApplication.java
@@ -0,0 +1,17 @@
+package no.responseweb.imagearchive.imageduplicatedetection;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class ImageDuplicateDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageDuplicateDetectionApplication.class, args);
+	}
+
+}
+
+
---
+++ src/test/java/no/responseweb/imagearchive/imageduplicatedetection/ImageDuplicateDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package no.responseweb.imagearchive.imageduplicatedetection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageDuplicateDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
