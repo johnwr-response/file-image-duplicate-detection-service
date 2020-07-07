fc7bd100c7b56517a0022e6e3d1c7941ab36b87e
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2020-07-07 18:08:09 +0200

    Restructured handling file processing in the file-store-service. Established into a file-processing-service, which in turn calls any other processing services commanded by detecting i.e. type and any other separation Established the FileEntity paradigm

--- src/main/java/no/responseweb/imagearchive/imageduplicatedetection/ImageDuplicateDetectionApplication.java
+++ src/main/java/no/responseweb/imagearchive/imageduplicatedetection/ImageDuplicateDetectionApplication.java
@@ -1,15 +1,19 @@
 package no.responseweb.imagearchive.imageduplicatedetection;
 
 import lombok.extern.slf4j.Slf4j;
+import nu.pattern.OpenCV;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @Slf4j
 @SpringBootApplication
+@EnableFeignClients
 public class ImageDuplicateDetectionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ImageDuplicateDetectionApplication.class, args);
+		OpenCV.loadShared();
 	}
 
 }
--- src/main/java/no/responseweb/imagearchive/imageduplicatedetection/services/ImageDuplicateDetectionJobListener.java
+++ src/main/java/no/responseweb/imagearchive/imageduplicatedetection/services/ImageDuplicateDetectionJobListener.java
@@ -7,12 +7,59 @@ import no.responseweb.imagearchive.model.ImageDuplicateDetectionJobDto;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
 @Slf4j
 @RequiredArgsConstructor
 @Component
 public class ImageDuplicateDetectionJobListener {
+
+    private final FileStoreFetcherService fileStoreFetcherService;
+
     @JmsListener(destination = JmsConfig.IMAGE_DUPLICATE_DETECTION_JOB_QUEUE)
-    public void listen(ImageDuplicateDetectionJobDto imageDuplicateDetectionJobDto) {
+    public void listen(ImageDuplicateDetectionJobDto imageDuplicateDetectionJobDto) throws IOException {
         log.info("Called with: {}", imageDuplicateDetectionJobDto);
+        if (imageDuplicateDetectionJobDto.getFileItemIdQuery()!=null) {
+            byte[] downloadedBytesQuery = fileStoreFetcherService.fetchFile(imageDuplicateDetectionJobDto.getFileItemIdQuery());
+            BufferedImage imageQuery = ImageIO.read(new ByteArrayInputStream(downloadedBytesQuery));
+            if (imageQuery!=null) {
+                // get dHash of all images in dataset
+
+/*
+Consider using Lire:
+https://github.com/dermotte/lire
+
+openimaj approach:
+                MBFImage query = ImageUtilities.readMBF(new ByteArrayInputStream(downloadedBytesQuery));
+                MBFImage target = ImageUtilities.readMBF(new ByteArrayInputStream(downloadedBytesTarget));
+
+                DoGSIFTEngine engine = new DoGSIFTEngine();
+                LocalFeatureList<Keypoint> queryKeypoints = engine.findFeatures(query.flatten());
+                LocalFeatureList<Keypoint> targetKeypoints = engine.findFeatures(target.flatten());
+
+                LocalFeatureMatcher<Keypoint> matcher = new BasicMatcher<Keypoint>(80);
+                matcher.setModelFeatures(queryKeypoints);
+                matcher.findMatches(targetKeypoints);
+
+                MBFImage basicMatches = MatchingUtilities.drawMatches(query, target, matcher.getMatches(), RGBColour.RED);
+                DisplayUtilities.display(basicMatches);
+
+                RobustAffineTransformEstimator modelFitter = new RobustAffineTransformEstimator(50.0, 1500,
+                        new RANSAC.PercentageInliersStoppingCondition(0.5));
+                matcher = new ConsistentLocalFeatureMatcher2d<Keypoint>(
+                        new FastBasicKeypointMatcher<Keypoint>(8), modelFitter);
+
+                matcher.setModelFeatures(queryKeypoints);
+                matcher.findMatches(targetKeypoints);
+
+                MBFImage consistentMatches = MatchingUtilities.drawMatches(query, target, matcher.getMatches(),
+                        RGBColour.RED);
+*/
+            }
+
+        }
     }
 }
